8b5f909588733257249940d5f222799dafb9e216
David <154616047+DavGam12@users.noreply.github.com>
2024-03-13 11:29:45 +0100

    Página Producto Finalizada
    Hemos creado el responsive además del JavaScript correspondiente para la funcionalidad de los botones

--- Producto/script.js
+++ Producto/script.js
@@ -22,3 +22,69 @@ menuButton.addEventListener("click", function(){
         categories.style.animationDuration = "2s"
     }
 })
+
+
+let loveIcon = document.getElementById("empty-love")
+let filledLoveIcon = document.getElementById("filled-love")
+
+loveIcon.addEventListener("click", function(){
+    loveIcon.style.display = "none"
+    filledLoveIcon.style.display = "block"
+})
+filledLoveIcon.addEventListener("click", function(){
+    loveIcon.style.display = "block"
+    filledLoveIcon.style.display = "none"
+})
+
+
+let addToCart = document.getElementById("add-to-cart")
+let addedToCart = document.getElementById("added-to-cart")
+let addToCartText = document.getElementById("add-to-cart-text")
+
+addToCart.addEventListener("click", function(){
+    addToCart.style.display = "none"
+    addedToCart.style.display = "block"
+    addToCartText.innerHTML = "Añadido"
+    addToCartText.style.marginLeft = "20px"
+})
+addedToCart.addEventListener("click", function(){
+    addToCart.style.display = "block"
+    addedToCart.style.display = "none"
+    addToCartText.innerHTML = "Añadir al carrito"
+    addToCartText.style.marginLeft = "0px"
+})
+
+let chairGrey = document.getElementById("chair-grey")
+let chairBeige = document.getElementById("chair-beige")
+let chairBlack = document.getElementById("chair-black")
+let chairWhite = document.getElementById("chair-white")
+
+let grey = document.getElementById("grey")
+let beige = document.getElementById("beige")
+let black = document.getElementById("black")
+let white = document.getElementById("white")
+
+grey.addEventListener("click", function(){
+    chairGrey.style.display = "block"
+    chairBeige.style.display = "none"
+    chairBlack.style.display = "none"
+    chairWhite.style.display = "none"
+})
+beige.addEventListener("click", function(){
+    chairGrey.style.display = "none"
+    chairBeige.style.display = "block"
+    chairBlack.style.display = "none"
+    chairWhite.style.display = "none"
+})
+black.addEventListener("click", function(){
+    chairGrey.style.display = "none"
+    chairBeige.style.display = "none"
+    chairBlack.style.display = "block"
+    chairWhite.style.display = "none"
+})
+white.addEventListener("click", function(){
+    chairGrey.style.display = "none"
+    chairBeige.style.display = "none"
+    chairBlack.style.display = "none"
+    chairWhite.style.display = "block"
+})
