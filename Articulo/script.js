104b509a26e00fc022747da36f1c7bdde0483f2b
David <154616047+DavGam12@users.noreply.github.com>
2024-03-13 11:27:55 +0100

    Página artículo terminada
    Añadido el JavaScript para la funcionalidad de varios botones además del responsive

--- Articulo/script.js
+++ Articulo/script.js
@@ -22,3 +22,79 @@ menuButton.addEventListener("click", function(){
         categories.style.animationDuration = "2s"
     }
 })
+
+
+let loveIcon = document.getElementsByClassName("empty-love")
+let loveIconFilled = document.getElementsByClassName("filled-love")
+
+loveIcon[0].addEventListener("click", function(){
+    loveIcon[0].style.display = "none"
+    loveIconFilled[0].style.display = "block"
+})
+loveIconFilled[0].addEventListener("click", function(){
+    loveIcon[0].style.display = "block"
+    loveIconFilled[0].style.display = "none"
+})
+
+loveIcon[1].addEventListener("click", function(){
+    loveIcon[1].style.display = "none"
+    loveIconFilled[1].style.display = "block"
+})
+loveIconFilled[1].addEventListener("click", function(){
+    loveIcon[1].style.display = "block"
+    loveIconFilled[1].style.display = "none"
+})
+
+loveIcon[2].addEventListener("click", function(){
+    loveIcon[2].style.display = "none"
+    loveIconFilled[2].style.display = "block"
+})
+loveIconFilled[2].addEventListener("click", function(){
+    loveIcon[2].style.display = "block"
+    loveIconFilled[2].style.display = "none"
+})
+
+loveIcon[3].addEventListener("click", function(){
+    loveIcon[3].style.display = "none"
+    loveIconFilled[3].style.display = "block"
+})
+loveIconFilled[3].addEventListener("click", function(){
+    loveIcon[3].style.display = "block"
+    loveIconFilled[3].style.display = "none"
+})
+
+loveIcon[4].addEventListener("click", function(){
+    loveIcon[4].style.display = "none"
+    loveIconFilled[4].style.display = "block"
+})
+loveIconFilled[4].addEventListener("click", function(){
+    loveIcon[4].style.display = "block"
+    loveIconFilled[4].style.display = "none"
+})
+
+loveIcon[5].addEventListener("click", function(){
+    loveIcon[5].style.display = "none"
+    loveIconFilled[5].style.display = "block"
+})
+loveIconFilled[5].addEventListener("click", function(){
+    loveIcon[5].style.display = "block"
+    loveIconFilled[5].style.display = "none"
+})
+
+loveIcon[6].addEventListener("click", function(){
+    loveIcon[6].style.display = "none"
+    loveIconFilled[6].style.display = "block"
+})
+loveIconFilled[6].addEventListener("click", function(){
+    loveIcon[6].style.display = "block"
+    loveIconFilled[6].style.display = "none"
+})
+
+loveIcon[7].addEventListener("click", function(){
+    loveIcon[7].style.display = "none"
+    loveIconFilled[7].style.display = "block"
+})
+loveIconFilled[7].addEventListener("click", function(){
+    loveIcon[7].style.display = "block"
+    loveIconFilled[7].style.display = "none"
+})
